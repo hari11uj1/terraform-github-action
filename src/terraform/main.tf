0f7c4a7c2bc64bbb0ba15f98bcb1fff08087f6f0
harish <yarraguntaharish93@gmail.com>
2023-01-09 13:04:46 +0530

    warehouse name changed

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -49,9 +49,9 @@ module "MARKETING_SMALL_WH_MOD_06" {
 }
 
 
-module "MARKETING_SMALL_WH_MOD_testrevert" {
+module "MARKETING_SMALL_WH_MOD_testrevert " {
   source            = "./warehouse"
-  warehouse_name    = "MARKETING_SMALL_WH_MOD_06"
+  warehouse_name    = "MARKETING_SMALL_WH_MOD_testrevert"
   warehouse_size    = "SMALL"
   roles = {
     "OWNERSHIP" = ["MARKETING"],

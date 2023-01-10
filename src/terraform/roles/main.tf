afe69b978f64f39454fb468f65b794f516332383
harish <yarraguntaharish93@gmail.com>
2023-01-10 17:03:47 +0530

    added role module

---
+++ src/terraform/roles/main.tf
@@ -0,0 +1,14 @@
+resource "snowflake_role" "ROLE" {
+  name = var.name
+  comment = var.comment
+}
+
+resource "snowflake_role_grants" "ROLE_GRANTS" {
+  role_name = snowflake_role.ROLE.name
+  roles = var.role_names
+  users = var.users
+}
+
+output "ROLE" {
+  value = snowflake_role.ROLE
+}

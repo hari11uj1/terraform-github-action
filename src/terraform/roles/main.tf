89aeaa4ac4e3612b58bebfdecf51b9b64763dbd2
harish <yarraguntaharish93@gmail.com>
2023-01-10 17:08:09 +0530

    added the provider to roles module

--- src/terraform/roles/main.tf
+++ src/terraform/roles/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.39.0"
+    }
+  }
+}
+
+
 resource "snowflake_role" "ROLE" {
   name = var.name
   comment = var.comment

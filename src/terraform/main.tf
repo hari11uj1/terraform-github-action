1fe359c803edb82a1b37b359961af2d915ba2d39
harish <yarraguntaharish93@gmail.com>
2023-01-10 16:34:58 +0530

    testing_users

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -106,6 +106,8 @@ module "MARKETING_DB_9" {
 }
 
 
+
+
 resource "snowflake_role" "ROLE" {
   name    = "TESTING"
   comment = "this role is for test env."
@@ -113,4 +115,32 @@ resource "snowflake_role" "ROLE" {
 
 output "MARKETING_DB" {
   value = module.MARKETING_DB_9
+}
+
+/*module "operations"{
+  source = "./roles"
+  name = "operations"
+  comment = "a read only role for operations"
+  role_names = ["ACCOUNTADMIN","OPERATION_DEVELOPER"]
+  users = [
+    module.ALL_USERS.USERS.TEST_USER1.name,
+    module.ALL_USERS.USERS.TEST_USER2.name,
+  ]
+}*/
+
+
+module "ALL_USERS_opp" {
+  source = "./users"
+
+  user_maps = {
+
+    "opp_USER1" : {"first_name" = "opp","last_name"="user1","email"="opp_user1@snowflake.example","default_warehouse"="MARKETING_SMALL_WH_MOD_07","default_role"="TESTING"},
+    "opp_USER2" : {"first_name" = "opp","last_name"="user2","email"="opp_user2@snowflake.example","default_warehouse"="MARKETING_SMALL_WH_MOD_06","default_role"="MARKETING"},
+    "opp_USER3" : {"first_name" = "opp","last_name"="user3","email"="opp_user3@snowflake.example"}
+  }
+}
+
+output "ALL_USERS_UAT" {
+  value = module.ALL_USERS_UAT
+  sensitive = true
 }

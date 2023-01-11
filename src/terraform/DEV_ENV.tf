6c0d0f62527ad8bc62f2162b7908ff3a1697d55a
harish <yarraguntaharish93@gmail.com>
2023-01-11 17:19:20 +0530

    ADDED DB_ADMIN

--- src/terraform/DEV_ENV.tf
+++ src/terraform/DEV_ENV.tf
@@ -35,8 +35,8 @@ module "ALL_USERS_DEV" {
 
   user_maps = {
 
-    "DEV_USER01" : {"first_name" = "DEV","last_name"="user1","email"="DEV_user1@snowflake.example","default_warehouse"="COMPUTE_WH","default_role"="PUBLIC"},
-    "DEV_USER02" : {"first_name" = "DEV","last_name"="user2","email"="DEV_user2@snowflake.example","default_warehouse"="COMPUTE_WH","default_role"="PUBLIC"},
+    "DEV_USER01" : {"first_name" = "DEV","last_name"="user1","email"="DEV_user1@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH04","default_role"="DB_ADMIN"},
+    "DEV_USER02" : {"first_name" = "DEV","last_name"="user2","email"="DEV_user2@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH04","default_role"="PUBLIC"},
     "DEV_USER03" : {"first_name" = "DEV","last_name"="user3","email"="DEV_user3@snowflake.example"}
   }
 }
@@ -48,7 +48,7 @@ output "ALL_USERS_DEV" {
 
 module "DEV_ROLES" {
  source = "./roles"
- name = "DEV_ROLES"
+ name = "DB_ADMIN"
  comment = "a role for SYSADMIN inc"
  role_name = ["SYSADMIN"]
  users = [

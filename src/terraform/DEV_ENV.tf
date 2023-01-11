d965bf0bfb21f2e8c7db93e1a717bb9469273219
harish <yarraguntaharish93@gmail.com>
2023-01-12 03:58:02 +0530

    ADDED HEIRARCHY TO DEV_ENV

--- src/terraform/DEV_ENV.tf
+++ src/terraform/DEV_ENV.tf
@@ -35,9 +35,9 @@ module "ALL_USERS_DEV_VIZ" {
 
   user_maps = {
 
-    "DEV_USER04" : {"first_name" = "DEV","last_name"="user1","email"="DEV_user1@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH05","default_role"="DATA_ANALYST"},
-    "DEV_USER05" : {"first_name" = "DEV","last_name"="user2","email"="DEV_user2@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH05","default_role"="DATA_ANALYST"},
-    "VIZ_USER01" : {"first_name" = "DEV","last_name"="user3","email"="DEV_user3@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH05","default_role"="DATA_ANALYST"}
+    "DEV_USER04" : {"first_name" = "DEV","last_name"="user1","email"="DEV_user1@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH05","default_role"="DATA_VIZ"},
+    "DEV_USER05" : {"first_name" = "DEV","last_name"="user2","email"="DEV_user2@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH05","default_role"="DATA_VIZ"},
+    "VIZ_USER01" : {"first_name" = "DEV","last_name"="user3","email"="DEV_user3@snowflake.example","default_warehouse"="WAREHOUSE_DEV_WH05","default_role"="DATA_VIZ"}
   }
 }
 
@@ -50,7 +50,7 @@ module "DATA_VIZ" {
  source = "./roles"
  name = "DATA_VIZ"
  comment = "a role for SYSADMIN inc"
- role_name = ["SYSADMIN","DATA_ENGG"]
+ role_name = ["DATA_ANALYST"]
  users = [
   module.ALL_USERS_DEV_VIZ.USERS.DEV_USER04.name,
   module.ALL_USERS_DEV_VIZ.USERS.DEV_USER05.name,
@@ -64,8 +64,8 @@ module "WAREHOUSE_DEV_WH05" {
   warehouse_name    = "WAREHOUSE_DEV_WH05"
   warehouse_size    = "SMALL"
   roles = {
-    "OWNERSHIP" = ["SYSADMIN"],
-    "USAGE" = ["SYSADMIN","DATA_ENGG"]
+    "OWNERSHIP" = ["DATA_ANALYST"],
+    "USAGE" = ["DATA_ANALYST","DATA_VIZ"]
   }
   with_grant_option = false
 }
@@ -77,15 +77,15 @@ module "DATABASE_DEV_DB05" {
   db_comment = "DATABASE FOR DEV_ENV_DB04"
   db_data_retention_time_in_days = 1
   db_role_grants = {
-    "OWNERSHIP" = ["SYSADMIN"],
-    "USAGE" = ["SYSADMIN","PUBLIC"]
+    "OWNERSHIP" = ["DATA_ANALYST"],
+    "USAGE" = ["DATA_ANALYST","DATA_VIZ"]
   }
   schemas = ["STAGE_SCHEMA","TARGET_SCHEMA",]
   schema_grants = {
-   "STAGE_SCHEMA OWNERSHIP" = {"roles"= ["SYSADMIN"]},
-   "STAGE_SCHEMA USAGE" = {"roles"= ["SYSADMIN","DATA_ENGG"]},
-   "TARGET_SCHEMA OWNERSHIP" = {"roles"= ["SYSADMIN"]},
-   "TARGET_SCHEMA USAGE"= {"roles"= ["SYSADMIN","DATA_ENGG"]}
+   "STAGE_SCHEMA OWNERSHIP" = {"roles"= ["DATA_ANALYST"]},
+   "STAGE_SCHEMA USAGE" = {"roles"= ["DATA_ANALYST","DATA_VIZ"]},
+   "TARGET_SCHEMA OWNERSHIP" = {"roles"= ["DATA_ANALYST"]},
+   "TARGET_SCHEMA USAGE"= {"roles"= ["DATA_ANALYST","DATA_VIZ"]}
   }
   
 }

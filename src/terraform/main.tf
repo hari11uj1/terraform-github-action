d0cf26b5046036bb7742d2654dc6d9122841e2fa
harish <yarraguntaharish93@gmail.com>
2023-01-11 09:35:19 +0530

    trile for tester

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -105,70 +105,12 @@ module "MARKETING_DB_9" {
   
 }
 
-
-module "DB_ADMIN"{
-  source = "./roles"
-  name = "DB_ADMIN"
-  comment = "a read only role for operations"
-  role_name = ["SYSADMIN"]
- /* users = [
-    module.ALL_USERS_DEV.USERS.DEV_USER1.name,
-    module.ALL_USERS_DEV.USERS.DEV_USER2.name,
-  ]*/
-}
-
-module "DATA_ENGG"{
-  source = "./roles"
-  name = "DATA_ENGG"
-  comment = "a read only role for operations"
-  role_name = [module.DB_ADMIN.name]
- /* users = [
-    module.ALL_USERS_DEV.USERS.DEV_USER1.name,
-    module.ALL_USERS_DEV.USERS.DEV_USER2.name,
-  ]*/
-}
-
-
-module "DATA_ANALYST"{
-  source = "./roles"
-  name = "DATA_ANALYST"
-  comment = "a read only role for operations"
-  role_name = [module.DB_ADMIN.name]
-  /*users = [
-    module.ALL_USERS_DEV.USERS.DEV_USER1.name,
-    module.ALL_USERS_DEV.USERS.DEV_USER2.name,
-  ]*/
-}
-
-module "DATA_LOADER"{
-  source = "./roles"
-  name = "DATA_LOADER"
-  comment = "a read only role for operations"
-  role_name = [module.DATA_ANALYST.name]
- /* users = [
-    module.ALL_USERS_DEV.USERS.DEV_USER1.name,
-    module.ALL_USERS_DEV.USERS.DEV_USER2.name,
-  ]*/
-}
-
-module "DATA_VIZ"{
-  source = "./roles"
-  name = "DATA_VIZ"
-  comment = "a read only role for operations"
-  role_name = [module.DATA_LOADER.name]
-  /*users = [
-    module.ALL_USERS_DEV.USERS.DEV_USER1.name,
-    module.ALL_USERS_DEV.USERS.DEV_USER2.name,
-  ]*/
-}
-
-
-
-
-
-resource "snowflake_role" "ROLE" {
-  name    = "TESTING"
-  comment = "this role is for test env."
+module "testerrole" {
+ source = "./roles"
+ name = "testerrole"
+ comment = "a role for testers inc"
+ role_name = ["SECURITYADMIN","MARKETING"]
+ users = module.ALL_USERS_opp
 }
 
 output "MARKETING_DB" {
